b6de56d95dca3d3d120b577f8c3f488e84f3086e
Isha Perry <toufgirl99@aol.com>
2020-12-07 13:52:50 -0800

    Update main.cpp

--- Assignments/P09/10327/main.cpp
+++ Assignments/P09/10327/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int Sort(int arr[], int len)
 {
   int swaps = 0;
-  for(int i = 0; i < len-1; i++)
+  for(int i = 0; i < len; i++)
   {
     for(int j = i+1; j < len; j++)
     {
